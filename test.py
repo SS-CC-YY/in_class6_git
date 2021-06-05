67d2f1c2f2c4e93ee56203ead52cadf8ed87e188
SS-CC-YY <songchen@oregonstate.edu>
2021-06-05 15:52:00 +0800

    added tests

---
+++ test.py
@@ -0,0 +1,12 @@
+import example
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(example.add(3,4), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
