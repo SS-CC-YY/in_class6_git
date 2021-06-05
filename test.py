494aaee87ee48aa502a1d32eb9fb6229f100bed2
SS-CC-YY <songchen@oregonstate.edu>
2021-06-05 16:43:58 +0800

    add tests

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ class TestCase(unittest.TestCase):
     def test_sub_1(self):
         self.assertEqual(example.subtract(4, 3), 1)
 
+    def test_mul_1(self):
+        self.assertEqual(example.mulityply(3, 4), 12)
+
+    def test_div_1(self):
+        self.assertEqual(example.divide(4, 2), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
